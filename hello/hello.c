48c2f913253cb44b0838a664d87a6a4c73231e9c
Connor Mahony <youremail@huskers.unl.edu>
2021-08-26 17:00:05 +0000

    added my major to the output

--- hello/hello.c
+++ hello/hello.c
@@ -1,2 +1,18 @@
 /**
- * 
+ * Author: Connor Mahony
+ *         cmahony2@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Connor Mahony!\n");
+  printf("Computer Engineering\n");
+
+  return 0;
+}
