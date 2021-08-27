7c7918c2cfed19ac0633f173521c4067366a65bb
Connor Mahony <youremail@huskers.unl.edu>
2021-08-27 22:51:29 +0000

    Created a Space between my major and partners name

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,10 @@
 /**
- * Author: Connor Mahony
+ * Author: Connor Mahony and Morgan McArthur
  *         cmahony2@huskers.unl.edu
- * Date: 2021/08/24
+ * 
+ * Date:   2021/08/27
  *
- * A simple hello world program in C
+ * A simple hello program in C stating name and major
  *
  */
 #include <stdlib.h>
@@ -12,7 +13,7 @@
 int main(int argc, char **argv) {
 
   printf("Connor Mahony!\n");
-  printf("Computer Engineering\n");
+  printf("Computer Engineering\n\n");
   
   printf("Morgan McArthur!\n");
   printf("Biological Systems Engineering\n");
